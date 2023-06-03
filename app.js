25c89737698a2d2147833a5096cbfb673f4e98ae
Lukasz <lukasz.maniak22@gmail.com>
2023-06-03 19:16:20 +0200

    main

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const msgElement = document.querySelector('.alert-box');
 const modalBox = document.querySelector('.modal');
 const closeModal = document.querySelector('.close-modal');
 
+//simple input check
 const checkInput = function () {
   let inputVal = input.value;
   let msg;
@@ -15,16 +16,18 @@ const checkInput = function () {
   if (!inputVal) {
     showMsg('Whoops! It looks like you forgot to add your email');
     msgElement.classList.remove('hidden');
-    //valid email adress
+    //wrong email adress
   } else if (!valid.test(inputVal)) {
     showMsg('Please provide a valid email address');
     msgElement.classList.remove('hidden');
   } else {
+    //correct email
     hideMsg();
     showModal();
   }
 };
 
+//setting input styles
 const setInputStyles = function (boolean) {
   if (boolean) {
     input.style.border = 'solid 2px var(--light-red)';
@@ -35,12 +38,13 @@ const setInputStyles = function (boolean) {
   }
 };
 
+//show modal message
 const showMsg = function (msg) {
   setInputStyles(true);
   msgElement.classList.add('alert');
   msgElement.innerText = msg;
 };
-
+//hide modal message
 const hideMsg = function () {
   setInputStyles(false);
   msgElement.classList.add('hidden');
@@ -49,12 +53,12 @@ const hideMsg = function () {
 input.addEventListener('focus', function () {
   input.style.color = 'black';
 });
-
+//show modal
 const showModal = function () {
   modalBox.classList.remove('hidden');
   modalBox.classList.add('drop');
 };
-
+//hide modal
 const hideModal = function () {
   modalBox.classList.add('hidden');
 };
